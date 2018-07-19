696c995a6ea199520a50ce0210625975904a9f6e
Muhammad <nhmishaq@gmail.com>
2018-07-19 00:46:45 -0400

    cleaned up the controller method to include instantiation of PayInfo object

--- paymerang/paymerang/Controllers/HomeController.cs
+++ paymerang/paymerang/Controllers/HomeController.cs
@@ -18,49 +18,8 @@ namespace paymerang.Controllers
         [Route("")]
         public IActionResult Index()
         {
-            string json =  @"{
-                'PayeeName': 'PayInfo.PayeeName',
-                'PayeeFax: 'PayInfo.PayeeFax',
-                'PayeePhone': 'PayInfo.PayeePhone' ,
-                'PaymentPAN': 'PayInfo.PaymentPAN',
-                'PaymentCVV': 'PayInfo.PaymentCVV',
-                'PaymentExp': 'PayInfo.PaymentExp',
-                'PayInfo.remittanceLines': [
-                    'RemittancePayorName': 'Remittance.RemittancePayorName',
-                    'RemittancePayorId': 'Remittance.RemittancePayorId',
-                    'RemittanceInvoiceNo': 'Remittance.RemittanceInvoiceNo',
-                    'RemittanceDescription': 'Remittance.RemittanceDescription',
-                    'RemittanceAmount': 'Remittance.RemittanceAmount',
-                ],
-                'PaymentAddress': [
-                    'Address1': 'PaymentAddress.Address1',
-                    'Address2': 'PaymentAddress.Address2',
-                    'city': 'PaymentAddress.city',
-                    'StateOrProvince': 'PaymentAddress.StateOrProvince',
-                    'Zip': 'PaymentAddress.Zip'
-                ]
-                }";
-            var result = JsonConvert.DeserializeObject<PayInfo>(json);
+            var result = new paymerang.JsonToFile<PayInfo>();
             return View(result);
         }
     }
 }
-
-
- // List<PayInfo> payee = JsonToFile<PayInfo>.ReadJson();
-            // List<PaymentAddress> payment = JsonToFile<PaymentAddress>.ReadJson();
-            // List<Remittance> remittance = JsonToFile<Remittance>.ReadJson();
-        //going to scrap ViewBag approach
-            // ViewBag.payees = from vendor in payee select new {
-            //     vendor.PayeeName, vendor.PayeeFax, vendor.PayeePhone, vendor.PayeeAddress
-            // };
-
-            // ViewBag.payments = from payData in payment select new {
-            //     payData.PaymentPAN, payData.PaymentCVV, payData.PaymentExp
-            // };
-
-            // ViewBag.remittance = from remitData in remittance select new {
-            //     remitData.RemittancePayorName, remitData.RemittancePayorId, remitData.RemittanceInvoiceNo, remitData.RemittanceDescription, remitData.RemittanceAmount
-            // };
-
-            // var result = paymerang.JsonToFile<PaymentAddress>.ReadJson();
