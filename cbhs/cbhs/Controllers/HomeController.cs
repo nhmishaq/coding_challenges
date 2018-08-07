6e1c849cda25aceb52c456cff3b21afd0ae0b7bd
Muhammad <nhmishaq@gmail.com>
2018-08-07 15:58:51 -0400

    tables

--- cbhs/cbhs/Controllers/HomeController.cs
+++ cbhs/cbhs/Controllers/HomeController.cs
@@ -16,17 +16,38 @@ namespace cbhs.Controllers
         {
             string viewAllPortals = $"Select * from u_portal order by portal.nm";
             // DbConnector.Query(viewAllPortals);
+            ViewBag.viewAllPortals = viewAllPortals;
             return View();
         }
 
-        // GET: /Home/
+        // Selecting a client should call to get list of courses for the selected client. Still needs a correct query for viewCourses
         [HttpGet]
-        [Route("register")]
-        public IActionResult Register()
+        [Route("clientCourseList")]
+        public IActionResult clientCourseList()
         {
-            // string executeRegistration = "Select p.portal_id, p.portal_nm from u_portal p order by p.portal_nm";
-            // DbConnector.Execute(executeRegistration);
+            // string viewCourses =  $"Select * from u_portal order by portal.nm";
+            // // DbConnector.Query(viewCourses);
+            // ViewBag.viewCourses = viewCourses;
+
+            string viewCourses = $"select registration_id, user_id, course_portal_id, registration_dttm from u_registration  where course_portal_id = course_portal_id_coming_in order by registration_dttm";
+            // DbConnector.Query(executeRegistration);
+            ViewBag.viewCourses = viewCourses;
             return View();
         }
+
+        // Selecting a course should bring up list of users registered to the course.
+
+        [HttpGet]
+        [Route("registeredUsersOfCourse")]
+        public IActionResult registeredUsersOfCourse(string course_portal_id_coming_in)
+        {
+            string registration_list = $"select registration_id, user_id, course_portal_id, registration_dttm from u_registration  where course_portal_id = course_portal_id_coming_in order by registration_dttm";
+            // DbConnector.Query(executeRegistration);
+            ViewBag.registration_list = registration_list;
+            return View();
+        }
+
+        
+
     }
 }
